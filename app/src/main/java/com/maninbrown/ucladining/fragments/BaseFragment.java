6052628603fcea9f73c1d5cd4b69264fa8584512
rahulkm <rahul.malavalli@gmail.com>
2015-09-13 01:03:51 -0700

    More complete in BaseFragment.

--- app/src/main/java/com/maninbrown/ucladining/fragments/BaseFragment.java
+++ app/src/main/java/com/maninbrown/ucladining/fragments/BaseFragment.java
@@ -1,6 +1,8 @@
 package com.maninbrown.ucladining.fragments;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
+import android.view.View;
 
 import com.maninbrown.ucladining.MainActivity;
 
@@ -11,10 +13,71 @@ import com.maninbrown.ucladining.MainActivity;
  */
 public abstract class BaseFragment extends Fragment {
 
-    public abstract void doRefresh();
+    private String mToolbarTitleText = "UCLA Dining";
+
+    private boolean mBackButtonIsOn = true;
+
+    private boolean mRefreshButtonIsOn = true;
+
+    // Toolbar options
+    protected void setToolbarTitle(String title) {
+        mToolbarTitleText = title;
+        refreshToolbarTitleText();
+    }
+
+    private void refreshToolbarTitleText() {
+        MainActivity mainActivity = getMainActivity();
+        if (mainActivity!=null) {
+            mainActivity.setToolbarTitle(mToolbarTitleText);
+            if (mToolbarTitleText.isEmpty()) {
+                mainActivity.setToolbarTitleVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+
+
+    protected void setBackButtonOn(boolean isOn) {
+        mBackButtonIsOn = isOn;
+        refreshBackButton();
+    }
+
+    private void refreshBackButton() {
+        MainActivity mainActivity = getMainActivity();
+        if (mainActivity!=null) {
+            mainActivity.toggleBackButton(mBackButtonIsOn);
+        }
+    }
+
+
+    protected void setRefreshButtonIsOn(boolean isOn) {
+        mRefreshButtonIsOn = isOn;
+        refreshToolbarRefreshButton();
+    }
+
+    private void refreshToolbarRefreshButton() {
+        MainActivity mainActivity = getMainActivity();
+        if (mainActivity!=null) {
+            mainActivity.toggleRefreshButton(mRefreshButtonIsOn);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshToolbarTitleText();
+        refreshBackButton();
+        refreshToolbarRefreshButton();
+    }
 
     protected MainActivity getMainActivity() {
-        return (MainActivity) getActivity();
+        Activity activity = getActivity();
+        return (activity == null) ? null : (MainActivity) activity;
     }
+
+
+    // Abstract methods
+
+    public abstract void doRefresh();
 
 }
